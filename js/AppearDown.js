2f085e666fdf3c245067a2198951accb32e2a72c
João Gabriel Petry de Brito <petry.brito@gmail.com>
2025-04-16 23:33:10 -0300

    Big changes

--- js/AppearDown.js
+++ js/AppearDown.js
@@ -1,13 +1,13 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('showD');
             observer.unobserve(entry.target);
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hiddenD');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// This JS code is responsible for making the boxes appear at the home Screen
+// Appearing thing down
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import './AppearRight.js';
+import './AppearDown.js';
+import './script.js'
+
+// Resposible for connecting all JS codes
